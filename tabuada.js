c9dc41f47ccc89036db15bd52a02079971af77ae
leandro <arcanjo.lm@gmail.com>
2022-06-05 11:39:51 -0300

    commit01

---
+++ tabuada.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * tabuada.js
+ */
+
+
+
+
+for(var i = 0; i <= 10 ; i++) {
+
+document.write ("<p>Tabuada do " + i + "</p>");
+for(var j = 0; j <= 10; j ++){
+
+    document.write(i + " x " + j + " = " + (i * j) +  "<br>");
+}
+      
+}
